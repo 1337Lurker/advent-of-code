412cee58e32d422e34a6e6af1153cf5e60f3e130
Gray Sanborn <gray.sanborn@daveramsey.com>
2022-12-02 14:54:32 -0600

    golfing

--- 2022/day2/part2.py
+++ 2022/day2/part2.py
@@ -1,33 +1,20 @@
 import fileinput
 
-ROCK, PAPER, SCISSORS = "A", "B", "C"
-RESPONSE_LOSE, RESPONSE_DRAW, RESPONSE_WIN = "X", "Y", "Z"
+ROCK, PAPER, SCISSORS, LOSE, DRAW, WIN = "A", "B", "C", "X", "Y", "Z"
 
-DRAW = {ROCK: ROCK, PAPER: PAPER, SCISSORS: SCISSORS}
-WIN = {ROCK: PAPER, PAPER: SCISSORS, SCISSORS: ROCK}
-LOSE = {ROCK: SCISSORS, PAPER: ROCK, SCISSORS: PAPER}
-
-SCORE = {ROCK: 1, PAPER: 2, SCISSORS: 3}
-
-
-def score_response(response):
-    return SCORE[response]
-
-
-def score_game_round(opponent, response) -> int:
-    if response == RESPONSE_DRAW:
-        return score_response(DRAW[opponent]) + 3
-    elif response == RESPONSE_WIN:
-        return score_response(WIN[opponent]) + 6
-    else:
-        return score_response(LOSE[opponent])
+RESPONSE = {
+    WIN: {ROCK: 8, PAPER: 9, SCISSORS: 7},
+    LOSE: {ROCK: 3, PAPER: 1, SCISSORS: 2},
+    DRAW: {ROCK: 4, PAPER: 5, SCISSORS: 6},
+}
 
 
 def main():
     score = 0
     with fileinput.input() as game_rounds:
         for game_round in game_rounds:
-            score += score_game_round(*game_round.strip().split(" "))
+            opponent, response = game_round.strip().split(" ")
+            score += RESPONSE[response][opponent]
 
     return score
 
