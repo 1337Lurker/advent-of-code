884fd51f7c23c4c382112ce889517df72e94067a
Gray Sanborn <gray.sanborn@daveramsey.com>
2022-12-02 15:47:47 -0600

    clarify variable names

--- 2022/day2/part2.py
+++ 2022/day2/part2.py
@@ -2,13 +2,17 @@ import fileinput
 
 ROCK, PAPER, SCISSORS, LOSE, DRAW, WIN = "A", "B", "C", "X", "Y", "Z"
 SCORE = {ROCK: 1, PAPER: 2, SCISSORS: 3, WIN: 6, LOSE: 0, DRAW: 3}
-RESPONSE = {
+OUTCOME_SCORE = {
     WIN: {
         ROCK: SCORE[PAPER] + SCORE[WIN],
         PAPER: SCORE[SCISSORS] + SCORE[WIN],
         SCISSORS: SCORE[ROCK] + SCORE[WIN],
     },
-    LOSE: {ROCK: SCORE[SCISSORS], PAPER: SCORE[ROCK], SCISSORS: SCORE[PAPER]},
+    LOSE: {
+        ROCK: SCORE[SCISSORS] + SCORE[LOSE],
+        PAPER: SCORE[ROCK] + SCORE[LOSE],
+        SCISSORS: SCORE[PAPER] + SCORE[LOSE],
+    },
     DRAW: {
         ROCK: SCORE[ROCK] + SCORE[DRAW],
         PAPER: SCORE[PAPER] + SCORE[DRAW],
@@ -21,8 +25,8 @@ def main():
     score = 0
     with fileinput.input() as game_rounds:
         for game_round in game_rounds:
-            opponent, response = game_round.strip().split(" ")
-            score += RESPONSE[response][opponent]
+            opponent_play, desired_outcome = game_round.strip().split(" ")
+            score += OUTCOME_SCORE[desired_outcome][opponent_play]
 
     return score
 
