229319e2ea9cb4020c2dbf7fae113f3b0e6f5b30
Gray Sanborn <gray@sanborn.pw>
2022-12-04 21:18:19 -0600

    Day 4 Part 1

---
+++ 2022/day4/part1.py
@@ -0,0 +1,28 @@
+import fileinput
+import string
+
+
+class Assignment:
+    @staticmethod
+    def parse(assignment: str):
+        assignment_start, _separator, assignment_stop = assignment.partition("-")
+        return set(range(int(assignment_start), int(assignment_stop) + 1))
+
+
+def main():
+    overlapped_assignments = 0
+    with fileinput.input() as assignments:
+        for assignment in assignments:
+            assignment_pair = assignment.strip().split(",")
+
+            first = Assignment.parse(assignment_pair[0])
+            second = Assignment.parse(assignment_pair[1])
+
+            if first.issuperset(second) or first.issubset(second):
+                overlapped_assignments += 1
+
+    return overlapped_assignments
+
+
+if __name__ == "__main__":
+    print(main())
