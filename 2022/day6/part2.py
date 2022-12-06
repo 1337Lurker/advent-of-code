16d8c7031c3535978243e58dc2fe45d82bc6a30e
Gray Sanborn <gray.sanborn@daveramsey.com>
2022-12-06 09:07:44 -0600

    Day 6 Part 2

---
+++ 2022/day6/part2.py
@@ -0,0 +1,18 @@
+import fileinput
+import re
+
+
+def main():
+    input_data = []
+    with fileinput.input() as file:
+        input_data = [line.rstrip("\n") for line in file]
+
+    signal = input_data[0]
+    for i in range(len(signal)):
+        header = set(signal[i:i+14])
+        if len(header) == 14:
+            return i+14
+
+
+if __name__ == "__main__":
+    print(main())
