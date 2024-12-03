a1d6059d1568b4d668d4a41210144dda5ae74657
Gray Sanborn <gray.sanborn@daveramsey.com>
2024-12-03 16:10:30 -0600

    2024 Day 3

--- 2024/day3/part1.py
+++ 2024/day3/part1.py
@@ -5,6 +5,17 @@ import re
 def main():
     raw_input = [re.sub("\n", "", line) for line in fileinput.input()]
 
+    arguments = []
+    for row in raw_input:
+        matches = re.findall(r"(mul\((\d{1,3}),(\d{1,3})\))", row)
+        for match in matches:
+            _instruction, multiplier, multiplicand = match
+            arguments.append((int(multiplier), int(multiplicand)))
+            continue
+
+    print(f"Sum of multiplications: {sum([a*b for a,b in arguments])}")
+    return
+
 
 if __name__ == "__main__":
     print(main())
