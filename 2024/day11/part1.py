647c1ce1ddda5931f4d800dce7f5b9f7f585df76
Gray Sanborn <gray.sanborn@daveramsey.com>
2024-12-18 10:08:28 -0600

    2024 Day 11

--- 2024/day11/part1.py
+++ 2024/day11/part1.py
@@ -5,6 +5,29 @@ import re
 def main():
     raw_input = [re.sub("\n", "", line) for line in fileinput.input()]
 
+    stones = raw_input[0].split()
+
+    # print(" ".join(stones))
+    for i in range(25):
+        stones = blink(stones)
+        # print(" ".join(stones))
+
+    return len(stones)
+
+def apply_rules(stone: str) -> list[str]:
+    digits = len(stone)
+    if stone == "0":
+        return ["1"]
+    elif len(stone) % 2 == 0:
+        return [stone[0:int(digits/2)], str(int(stone[int(digits/2):digits]))]
+    else:
+        return[str(int(stone)*2024)]
+
+def blink(stones: list[str])-> list[str]:
+    new_stones = []
+    for stone in stones:
+        new_stones.extend(apply_rules(stone))
+    return new_stones
 
 if __name__ == "__main__":
     print(main())
